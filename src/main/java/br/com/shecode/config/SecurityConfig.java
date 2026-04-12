17fa3e661b75cb065d640651e4cb67176abd6407
Gabriela Carvalho <gabrielacarvalho.zip@hotmail.com>
2026-04-12 00:31:03 -0300

    fix: adiciona OPTIONS e localhost:3000 no CORS

--- src/main/java/br/com/shecode/config/SecurityConfig.java
+++ src/main/java/br/com/shecode/config/SecurityConfig.java
@@ -36,9 +36,10 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "https://plataforma-para-mulheres-em-tech.vercel.app",
-                "http://localhost:8080"
+                "http://localhost:8080",
+                "http://localhost:3000"
         ));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         return new UrlBasedCorsConfigurationSource() {{
