975202914a58ff63bc254a393d36fad0e2aab4b2
Gabriela Carvalho <gabrielacarvalho.zip@hotmail.com>
2026-04-22 22:31:54 -0300

    fix: corrige login para e-mail.

--- src/main/java/br/com/shecode/config/SecurityConfig.java
+++ src/main/java/br/com/shecode/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,6 +23,9 @@ public class SecurityConfig {
                 .securityMatcher("/auth/**", "/trails/**", "/forum/**", "/users/**")
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 );
